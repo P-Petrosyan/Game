2feb46873f53b9a6d4fe1f02358f34ff4a50df22
Petros Petrosyan <petrospetrosyann@gmail.com>
2025-10-07 21:16:55 +0400

    07-10-25 Update for EAS build for Android

--- app.config.js
+++ app.config.js
@@ -4,22 +4,44 @@ export default {
     expo: {
         name: "Path Blocker Online",
         slug: "path-blocker",
-        version: "1.0.0",
+        version: "1.0.0", // App Store / Play Store visible version
         orientation: "portrait",
         icon: "./assets/images/icon1.png",
         scheme: "pathblocker",
         userInterfaceStyle: "automatic",
         newArchEnabled: true,
 
+        extra: {
+            eas: {
+                projectId: "8cc79ff7-348b-4fd6-a054-a627a95f87a3",
+            },
+            admob: {
+                androidAppId: "ca-app-pub-4468002211413891~1278388928",
+                iosAppId: "ca-app-pub-4468002211413891~5060585795",
+            },
+        },
+
         ios: {
             supportsTablet: true,
             bundleIdentifier: "com.anonymous.pathblocker",
             deploymentTarget: "18.0",
+            buildNumber: "1.0.0",
             googleServicesFile: "./GoogleService-Info.plist",
+            infoPlist: {
+                NSUserTrackingUsageDescription:
+                    "This identifier will be used to deliver personalized ads to you.",
+            },
         },
 
         android: {
             package: "com.anonymous.pathblocker",
+            versionCode: 1,
+            adaptiveIcon: {
+                foregroundImage: "./assets/images/icon1.png",
+                backgroundColor: "#E6F4FE",
+            },
+            edgeToEdgeEnabled: true,
+            predictiveBackGestureEnabled: false,
         },
 
         plugins: [
@@ -39,5 +61,10 @@ export default {
                 },
             ],
         ],
+
+        experiments: {
+            typedRoutes: true,
+            reactCompiler: true,
+        },
     },
 };
